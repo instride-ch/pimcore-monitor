d565120542fbf250ed68ef25d542f8d7153ddf06
Aaron Gerig <a.gerig@w-vision.ch>
2022-02-08 09:56:54 +0100

    feat: added default deployer recipe

---
+++ deployer/monitor.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Pimcore Monitor
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2022 w-vision AG (https://www.w-vision.ch)
+ * @license    https://github.com/w-vision/PimcoreMonitorBundle/blob/master/gpl-3.0.txt GNU General Public License version 3 (GPLv3)
+ */
+
+namespace Deployer;
+
+task('pimcore:monitor:health-report', static function () {
+    run("cd {{release_path}} && {{bin/console}} pimcore:monitor:health-report");
+});
+
+after('deploy:success', 'pimcore:monitor:health-report');
