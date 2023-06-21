46903ccdf82d623642be5bb7b6ee72c2b277d78f
aarongerig <a.gerig@w-vision.ch>
2023-06-21 17:41:32 +0200

    refactor: improve stability on https_connection check

--- src/PimcoreMonitorBundle/Check/HttpsConnection.php
+++ src/PimcoreMonitorBundle/Check/HttpsConnection.php
@@ -22,6 +22,7 @@ use Laminas\Diagnostics\Result\ResultInterface;
 use Laminas\Diagnostics\Result\Skip;
 use Laminas\Diagnostics\Result\Success;
 use Laminas\Diagnostics\Result\Warning;
+use Spatie\SslCertificate\SslCertificate;
 
 class HttpsConnection extends AbstractCheck
 {
@@ -44,24 +45,9 @@ class HttpsConnection extends AbstractCheck
             return new Warning('HTTPS encryption could not be checked');
         }
 
-        // Create a stream context
-        $stream = \stream_context_create(['ssl' => ['capture_peer_cert' => true]]);
-        $url = \sprintf('https://%s', $host);
+        $certificate = SslCertificate::createForHostName($host);
 
-        try {
-            // Bind the resource $url to $stream
-            $read = \fopen($url, 'rb', false, $stream);
-
-            // Get the stream parameters
-            $params = \stream_context_get_params($read);
-        } catch (\Exception) {
-            // Ignore exceptions thrown ...
-        }
-
-        // Check if SSL certificate is present
-        $cert = $params['options']['ssl']['peer_certificate'] ?? null;
-
-        if (null === $cert) {
+        if ($certificate->isValid() === false) {
             return new Failure('HTTPS encryption not activated', false);
         }
 
